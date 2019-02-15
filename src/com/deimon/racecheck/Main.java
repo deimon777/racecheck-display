5d7fa10b0e61f7f74ba11eba1f161a1d7a89e77b
deimon777 <blanco.damian@gmail.com>
2019-02-15 13:32:24 -0300

    Inicia la aplicacion

--- src/com/deimon/racecheck/Main.java
+++ src/com/deimon/racecheck/Main.java
@@ -7,27 +7,14 @@ import com.deimon.racecheck.gui.MainGUI;
 
 public class Main<T> extends Application {
 
-//		//		Path path = FileSystems.getDefault().getPath(System.getProperty("user.home"));
-//		Path path = FileSystems.getDefault().getPath(Statics.ruta_archivo);
-//		System.out.println(path.toString()); 
-//
-//
-//		// busco mi archivo
-//		//		File source = new File(Statics.nombre_archivo);
-//		//		System.out.println(source.getPath().toString());
-//		// creo el archivo temporal
-//		//		File dest = new File(Statics.nombre_archivo_temp);
-//		// copio la info del archivo en el temporal
-//		//		UtilsFile.copyFile(source, dest);
-//
-//		//		UtilsFile.compareFile(source, dest);
-	
 	@Override
-	public void start(Stage primaryStage) throws Exception {
-		MainGUI mainGui = new MainGUI();
-		mainGui.show();
+	public void start(Stage primaryStage) throws Exception {		
+		MainGUI gui = new MainGUI();
+	    System.out.println("El directorio temporal del sistema es "
+	    	      + System.getProperty("java.io.tmpdir"));
+		gui.show();
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);		
 	}
