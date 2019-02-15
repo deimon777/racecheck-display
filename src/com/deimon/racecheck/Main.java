7a0c5bb2b427ef9cc82ca7bbfd974492b5d3806c
deimon777 <blanco.damian@gmail.com>
2019-02-15 14:02:14 -0300

    Quitado un prinln

--- src/com/deimon/racecheck/Main.java
+++ src/com/deimon/racecheck/Main.java
@@ -10,8 +10,7 @@ public class Main<T> extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception {		
 		MainGUI gui = new MainGUI();
-	    System.out.println("El directorio temporal del sistema es "
-	    	      + System.getProperty("java.io.tmpdir"));
+	    // System.out.println("El directorio temporal del sistema es "+ System.getProperty("java.io.tmpdir"));
 		gui.show();
 	}
 
