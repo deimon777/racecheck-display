9c53dd1f47641e162823b255e2da57edf0852477
deimon777 <blanco.damian@gmail.com>
2019-02-23 13:43:36 -0300

    Rehecha la funcion de copiar, y codificando los caracteres para que el display lo muestre bien :)

--- src/com/deimon/racecheck/utils/UtilsFile.java
+++ src/com/deimon/racecheck/utils/UtilsFile.java
@@ -1,10 +1,13 @@
 package com.deimon.racecheck.utils;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.nio.file.FileAlreadyExistsException;
-import java.nio.file.Files;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
@@ -12,53 +15,72 @@ import java.util.Scanner;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 
-
 public class UtilsFile {
-	public File source;
-	public File dest;
+	// public File source;
+	// public File dest;
+
+	//	public void copyFile(File source, File dest){
+	//
+	//		System.out.println("ARCHIVO TEMPORAL: "+dest);
+	//		System.out.println("Arranco a copiar");
+	//		try {
+	//			Files.deleteIfExists(Paths.get(dest.toURI()));
+	//			Files.copy(source.toPath(), dest.toPath(),StandardCopyOption.REPLACE_EXISTING);
+	//		} catch (IOException e) {
+	////			Alert alert = new Alert(AlertType.ERROR);
+	////			alert.setTitle("Error - WatchFile (run)");
+	////			alert.setHeaderText(null);
+	////			alert.setContentText("Ooops!!! No se pudo copiar el archivo - WatchFile (run)");
+	////			alert.showAndWait();
+	//			e.printStackTrace();
+	//		}
+	//		System.out.println("Termino a copiar");
+	//	}
+
 
 	public void copyFile(File source, File dest) {
-		Alert alert = new Alert(AlertType.ERROR);
-		alert.setTitle("Error");
-		alert.setHeaderText(null);
+		InputStream is = null;
+		OutputStream os = null;
 		try {
-			Files.copy(source.toPath(), dest.toPath());
-		} catch (FileAlreadyExistsException e) {
-			System.out.println("El archivo ya existe");
-			dest.delete();
-			try {
-				Files.copy(source.toPath(), dest.toPath());
-			} catch (IOException e1) {
-				alert.setContentText("Ooops!!! El archivo existia y no se pudo copiar");
-				alert.showAndWait();
-				// e1.printStackTrace();
-				System.out.println("Error: " + e1.toString());
+			is = new FileInputStream(source);
+			os = new FileOutputStream(dest);
+			byte[] buffer = new byte[1024];
+			int length;			
+			while ((length = is.read(buffer)) > 0) {
+				os.write(buffer, 0, length);
 			}
-		} catch (IOException e) {
-			alert.setContentText("Ooops!!! Error desconocido, reiniciar");
-			alert.showAndWait();
-			// e.printStackTrace();
-			System.out.println("Error: " + e.toString());
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}catch (IOException e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				is.close();
+				os.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 
-	public Map<String, String> compareFile(File source, File dest) {
-		Map<String, String> out = null;
-		String first = "", second = "";
+	public Map<String, String> compareFile(File source, File dest){
 		Scanner input1 = null;
 		Scanner input2 = null;
-
-		Alert alert = new Alert(AlertType.ERROR);
-		alert.setTitle("Error");
-		alert.setHeaderText(null);
+		
+		Map<String, String> out = null;
+		String first = "", second = "";
 		try {
 			input1 = new Scanner(source);
 			input2 = new Scanner(dest);
-		} catch (FileNotFoundException e) {			
-			alert.setContentText("Ooops!!! Archivo no encontrado");
+		} catch (FileNotFoundException e) {
+			Alert alert = new Alert(AlertType.ERROR);
+			alert.setTitle("Error - UF (compare)");
+			alert.setHeaderText(null);
+			alert.setContentText(e.toString());
 			alert.showAndWait();
-			// e.printStackTrace();
-			System.out.println("Error: " + e.toString());
+			e.printStackTrace();
+			// System.out.println("Error (UF compare): " + e.toString());
 		}
 
 		while (input1.hasNextLine() || input2.hasNextLine()) {
@@ -69,7 +91,7 @@ public class UtilsFile {
 				second = input2.nextLine();
 			}
 			if (!first.equals(second)) {
-				//	System.out.println("Differences found: " + "\nF:" + first + "\nS:" + second);
+				System.out.println("Diferencias: " + "\nF:" + first + "\nS:" + second+ "\n-------");
 				out = this.parseText(first);
 				break;
 			}
@@ -85,16 +107,42 @@ public class UtilsFile {
 		String[] parts = txt.split("\\|");
 		Map<String, String> out = new HashMap<String, String>();
 		//;SEXO|NOMBRE|CHIP|DORSAL|MODALIDAD|CATEGORIA|TIEMPO|POSICION|POS.CAT.|RITMO
-		out.put("sexo",parts[0]);
-		out.put("nombre",parts[1]);
-		out.put("chip",parts[2]);
-		out.put("dorsal",parts[3]);
-		out.put("modalidad",parts[4]);
-		out.put("categoria",parts[5]);
-		out.put("tiempo",parts[6]);
-		out.put("posicion",parts[7]);
-		out.put("pos.cat",parts[8]);
-		out.put("ritmo",parts[9]);
+		try {
+			String charsetTo = "ISO-8859-1";
+			String charsetFrom = "windows-1252";
+			String aux = new String(parts[0].getBytes(charsetTo), charsetFrom);
+			out.put("sexo",aux);
+			
+			aux = new String(parts[1].getBytes(charsetTo), charsetFrom);			
+			out.put("nombre",aux);
+			
+			aux = new String(parts[2].getBytes(charsetTo), charsetFrom);
+			out.put("chip",aux);
+			
+			aux = new String(parts[3].getBytes(charsetTo), charsetFrom);
+			out.put("dorsal",aux);
+			
+			aux = new String(parts[4].getBytes(charsetTo), charsetFrom);
+			out.put("modalidad",aux);
+			
+			aux = new String(parts[5].getBytes(charsetTo), charsetFrom);
+			out.put("categoria",aux);
+			
+			aux = new String(parts[6].getBytes(charsetTo), charsetFrom);
+			out.put("tiempo",aux);
+			
+			aux = new String(parts[7].getBytes(charsetTo), charsetFrom);
+			out.put("posicion",aux);
+			
+			aux = new String(parts[8].getBytes(charsetTo), charsetFrom);
+			out.put("pos.cat",aux);
+			
+			aux = new String(parts[9].getBytes(charsetTo), charsetFrom);
+			out.put("ritmo",aux);
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return out;
 	}
 
